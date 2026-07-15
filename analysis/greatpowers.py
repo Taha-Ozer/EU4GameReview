1e574410e56b519cf4b68d377af8ee4e9a213743
Taha Ozer <tahaozer0@hotmail.com>
2026-07-15 12:02:46 +0200

    add comments to getGreatPowers()

--- analysis/greatpowers.py
+++ analysis/greatpowers.py
@@ -2,9 +2,15 @@ import country
 
 
 def getGreatPowers(data: dict, localisations: dict) -> list:
-    great_powers_og: dict = data.get("great_powers", {})
-    great_powers: dict = great_powers_og.get("original", {})
-    result: list = []
-    for entry in great_powers:
-        result.append(country.getCountry(entry.get("country"), data, localisations))
+    great_powers_og: dict = data.get(
+        "great_powers", {}
+    )  # we get the great_powers key's value
+    great_powers: dict = great_powers_og.get(
+        "original", {}
+    )  # that value is "original" so we get the value of "original"
+    result: list = []  # this will be our result list
+    for entry in great_powers:  # for every GP entry in the great_powers dict
+        result.append(
+            country.getCountry(entry.get("country"), data, localisations)
+        )  # we add it to the list as a country
     return result
